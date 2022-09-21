3659f45da3cdfecc3b14dfcbe250f5ef7f60ee86
Javier Solis <111769866+javiepic@users.noreply.github.com>
2022-09-21 15:32:15 +0000

    Step 4

--- .ccls-cache/@home@runner@BreakPractice/main.cpp
+++ .ccls-cache/@home@runner@BreakPractice/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  int dataTemp;
+  //load input file 
+  ifstream infile; //declare stream variable
+  infile.open("NumberData.txt");
+
+  //load all data in while loop
+  while (!infile.eof())
+    {
+      infile >> dataTemp;
+      cout << dataTemp << endl;
+
+      
+    }
+  
 }
